323673b8f8f38ce7554f77c825c402f0cabfd69e
dany <danyalexandr@gmail.com>
2023-01-16 19:34:11 -0300

    parch para imagenes

--- src/main/java/com/portfolio/backend/filter/CORSFilter.java
+++ src/main/java/com/portfolio/backend/filter/CORSFilter.java
@@ -24,20 +24,21 @@ public class CORSFilter implements Filter{
     
     @Override
     public void init(FilterConfig filterConfig) throws ServletException{
+	    
 }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException{ 
             HttpServletRequest request = (HttpServletRequest) req;
             HttpServletResponse response = (HttpServletResponse) res;
-            System.out.println(request.getMethod() + "CORSFilter HTTP request:");
+           
             
         response.addHeader("Access-Control-Max-Age", "-1");
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
         response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
-        
+         
         if("OPTIONS".equalsIgnoreCase(request.getMethod())){
             response.setStatus(HttpServletResponse.SC_OK);
         }else{
@@ -48,5 +49,6 @@ public class CORSFilter implements Filter{
     
     @Override
     public void destroy(){
+    
     }
 }
