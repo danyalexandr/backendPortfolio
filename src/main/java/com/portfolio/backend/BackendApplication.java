a5729b32d461bc42f16c453bbd06e42409a8d874
dany <danyalexandr@gmail.com>
2023-03-30 19:45:08 -0300

    clase en metoodo main para corregir un error

--- src/main/java/com/portfolio/backend/BackendApplication.java
+++ src/main/java/com/portfolio/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.portfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -12,6 +15,14 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
         
+	@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
 
         }
 
