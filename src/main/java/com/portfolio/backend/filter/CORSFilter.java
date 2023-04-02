7ff107ed6dd304fffa62dd7b4dfafe1fc7cb6003
dany <danyalexandr@gmail.com>
2023-04-02 17:27:19 -0300

    editado el controlador de educacion y el cors filter

--- src/main/java/com/portfolio/backend/filter/CORSFilter.java
+++ src/main/java/com/portfolio/backend/filter/CORSFilter.java
@@ -17,40 +17,41 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 
+
+
+// ... (imports) ...
+
 @Configuration
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CORSFilter implements Filter{
-    
+public class CORSFilter implements Filter {
+
     static Logger logger = LoggerFactory.getLogger(CORSFilter.class);
-    
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException{
-	    
-}
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException{ 
-            HttpServletRequest request = (HttpServletRequest) req;
-            HttpServletResponse response = (HttpServletResponse) res;
-           
-            
-        response.addHeader("Access-Control-Max-Age", "-1");
-        response.addHeader("Access-Control-Allow-Origin", "*");
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Max-Age", "-1");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.addHeader("Access-Control-Allow-Methods", "GET, DELETE, HEAD, PUT, POST, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization, *");
-         
-        if("OPTIONS".equalsIgnoreCase(request.getMethod())){
+        response.setHeader("Access-Control-Allow-Methods", "GET, DELETE, HEAD, PUT, POST, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
-        }else{
+        } else {
             chain.doFilter(req, res);
         }
         logger.info(request.getRemoteAddr());
     }
-    
+
     @Override
-    public void destroy(){
-    
+    public void destroy() {
     }
 }
